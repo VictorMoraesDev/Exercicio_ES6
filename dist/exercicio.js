b66caf3c8a438ba12f5c342f7c9e8517022d5388
Victor Moraes <victorf.moraes@hotmail.com>
2024-04-19 12:19:27 -0300

    commitando o exercicio

---
+++ dist/exercicio.js
@@ -0,0 +1,19 @@
+const alunos = [{
+  nome: "João",
+  nota: 4.5
+}, {
+  nome: "Maria",
+  nota: 9.0
+}, {
+  nome: "Pedro",
+  nota: 5.5
+}, {
+  nome: "Joaquina",
+  nota: 8.0
+}];
+console.log(alunos);
+function alunosAprovados(arrayDeAlunos) {
+  return arrayDeAlunos.filter(aluno => aluno.nota >= 6);
+}
+const alunosAprovadosArray = alunosAprovados(alunos);
+console.log(alunosAprovadosArray);
